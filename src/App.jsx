99915d105dcb1f53477b246c876c78fcae8c790c
Tansimrinky <tansimrinkybindu@gmail.com>
2024-06-25 22:13:30 +0600

    basic set up done 2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Bistro Boss Client</h1>
+      <button className="btn">Button</button>
+    </>
+  )
+}
+
+export default App
